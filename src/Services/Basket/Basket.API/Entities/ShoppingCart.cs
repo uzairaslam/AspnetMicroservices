53e362aa5b5c438a93b09c22fbde62e61c9fad13
uzairaslam <uzairaslam64@gmail.com>
2023-02-18 13:47:30 +0500

    Basket.API developed

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string username)
+        {
+            UserName = username;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                return Items == null || Items.Count() == 0 ? 0 : Items.Sum(i => i.Price);
+
+            }
+        }
+    }
+}
